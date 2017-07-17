b4bf8b27c3b2141fa55b703eb2201ea545b68b12
Marcos Rosada <marcos.rosada@rustcon.com.br>
2017-07-17 17:02:20 -0300

    Employee Form

--- src/app/employees/employee-form/employee-form.component.ts
+++ src/app/employees/employee-form/employee-form.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeFormComponent implements OnInit {
 
+  private employee: any = {
+    id: 1, name: 'Marcos', func: 'FED', company: 'Halliburton'
+  };
+  
   constructor() { }
 
   ngOnInit() {
