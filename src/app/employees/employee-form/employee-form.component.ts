f03f5eafddda7d30501dbd448c569014ce8c75b7
Marcos Rosada <marcos.rosada@rustcon.com.br>
2017-07-17 16:54:12 -0300

    Employee Form

--- src/app/employees/employee-form/employee-form.component.ts
+++ src/app/employees/employee-form/employee-form.component.ts
@@ -12,4 +12,9 @@ export class EmployeeFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit(form) {
+    console.log(form);
+    
+  }
+
 }
--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { EmployeesRoutingModule } from './employees.routing.module';
 import { EmployeesService } from './employees.service';
@@ -9,6 +10,7 @@ import { EmployeeFormComponent } from './employee-form/employee-form.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     EmployeesRoutingModule
   ],
   declarations: [
